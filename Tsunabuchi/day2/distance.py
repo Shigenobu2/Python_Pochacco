9e20668dbbb1824272132518ac6311752a0f7317
FukaTsunabuchi <tsunabuchi.fuka.ns@mynavi.jp>
2024-05-15 06:57:11 +0000

    day2

---
+++ Tsunabuchi/day2/distance.py
@@ -0,0 +1,11 @@
+import sys
+args = sys.argv
+station1 = args[1]
+station2 = args[2]
+
+distance = {'東京': 0.00, '品川': 6.78, '新横浜': 25.54, '名古屋': 342.02, '京都': 476.31, '新大阪': 515.35}
+
+between = 0
+between = abs(distance[station1] - distance[station2])
+
+print(str(round(between,2)),end="")
